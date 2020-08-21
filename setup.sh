217d4a5448e662d155cfd28487921faa2d8c187d
nomutin <nomura0508@icloud.com>
2020-08-21 20:53:28 +0900

    Install sublime(Instead of Vim) and optimize setup.sh

--- setup.sh
+++ setup.sh
@@ -7,54 +7,22 @@ if !(type 'brew' > /dev/null 2>&1); then
     brew tap caskroom/cask
 fi	
 
-#"===== install nightly-itrem2 ====="
-if !([ -e /Applications/iTerm.app ]); then
-    open -a 'Safari' "https://www.iterm2.com/nightly/latest"
-     % https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Hack.zip
-fi
-
-#"===== dein install ====="
-if !([ -e ~/.vim/dein/repos/github.com/Shougo/dein.vim ]); then
-    mkdir -p ~/.vim/dein/repos/github.com/Shougo/dein.vim
-    git clone https://github.com/Shougo/dein.vim.git ~/.vim/dein/repos/github.com/Shougo/dein.vim
-    echo 'dein.vim install'
-fi
-
-#"===== make dotfiles alias ====="
-DOT_FILES=(.vimrc .gitconfig .zshrc .latexmkrc )
-for file in ${DOT_FILES[@]};
-do
-    if !([ -e $HOME/$file ]); then
-        ln -s $HOME/dotfiles/$file $HOME/$file
-    fi
-done
-
-#"===== vim settings ====="
-VIM_OPTIONS=(rc colors)
-for option in ${VIM_OPTIONS[@]};
-do
-    if !([ -e $HOME/.vim/$option/ ]); then
-         mkdir $HOME/.vim/$option
-    fi
-
-    for file in $(ls $HOME/dotfiles/.vim/$option);
-        do
-            ln -s $HOME/dotfiles/.vim/$option/$file $HOME/.vim/$option/$file
-        done
-done
-
-#"===== install pyenv =====
-if !(type 'pyenv' > /dev/null 2>&1); then
-    brew install pyenv docker
-fi	
+#"===== brew install functions ====="
+brew install pyenv docker
 
 #"===== startup apps =====
-if !([ -e /Applications/clipy.app ]); then
-    brew cask install clipy coteditor skim docker
-fi
+brew cask install iterm2 clipy coteditor skim docker
+
+#"===== make dotfiles alias ====="
+DOT_FILES=(.gitconfig .zshrc .latexmkrc)
+for file in ${DOT_FILES[@]};
+    ln -s $HOME/dotfiles/$file $HOME/$file
+done
 
 #"===== setting for matplotlib =====
 mkdir $HOME/.matplotlib
- ln -s $HOME/dotfiles/matplotlibrc $HOME/.matplotlib/matplotlibrc
+ ln -sf $HOME/dotfiles/matplotlibrc $HOME/.matplotlib/matplotlibrc
  
+ #"===== MacBook settings =====
+defaults write "Apple Global Domain" com.apple.mouse.scaling 10
 defaults write com.apple.dock autohide-time-modifier -int 0 && killall Dock
