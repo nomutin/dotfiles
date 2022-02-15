0617df5b172267b2ba5b82385cb145264027b404
nomutin <nomura0508@icloud.com>
2022-02-15 21:22:03 +0900

    デフォルト設定自動化

--- setup.sh
+++ setup.sh
@@ -8,15 +8,33 @@ if !(type 'brew' > /dev/null 2>&1); then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi	
 
-#"===== startup apps =====
-brew install --cask iterm2 clipy deepl coteditor alfred swiftdefaultappsprefpane visual-studio-code rectangle keyboardcleantool dozer mathpix-snipping-tool google-drive slack zoom
-
 #"===== make dotfiles alias ====="
-DOT_FILES=(.gitconfig .zshrc .gitignore .vimrc .vim .latexmkrc)
+DOT_FILES=(.gitconfig .zshrc .gitignore .vimrc .vim .latexmkrc .zshenv)
 for file in ${DOT_FILES[@]};
 do
     ln -sf $HOME/dotfiles/$file $HOME/$file
 done
+ 
+#"===== startup apps =====
+brew install --cask iterm2 clipy deepl coteditor alfred swiftdefaultappsprefpane visual-studio-code rectangle keyboardcleantool dozer mathpix-snipping-tool google-drive slack zoom
 
 #"===== MacBook settings =====
-defaults write com.apple.dock autohide-time-modifier -int 0 && killall Dock
+defaults write com.apple.dock autohide-time-modifier -int 0 
+defaults write com.apple.dock autohide -bool true
+defaults write -g com.apple.trackpad.scaling 3
+defaults write -g com.apple.trackpad.tapBehavior -int 1
+brew install blacktop/tap/lporg
+# open /Applications/Clipy.app
+# open /Applications/DeepL.app
+# open /Applications/CotEditor.app
+# open /Applications/"Alfred 4.app"
+# open /Applications/"Visual Studio Code.app"
+# open /Applications/Rectangle.app
+# open /Applications/KeyboardCleanTool.app
+# open /Applications/"Mathpix Snipping Tool.app"
+# open /Applications/"Google Drive.app"
+# open /Applications/Slack.app
+# open /Applications/zoom.us.appss
+killall Finder
+killall Dock
+# sudo reboot
