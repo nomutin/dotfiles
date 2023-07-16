6c34dd1fef9380f8bcde6554579689c2476037dd
nomutin <nomura0508@icloud.com>
2023-07-16 16:23:41 +0900

    [Fix] 重いログを削除

--- texSetup.sh
+++
@@ -1,9 +0,0 @@
-# Apple Silicon用 Latexセットアップファイル
-
-# 事前設定
-brew install ghostscript
-
-# basictexのインストール
-brew install --cask basictex
-sudo tlmgr update --self --all
-sudo tlmgr install paper a4 latexmk collection-langjapanese collection-fontsrecommended collection-fontutils listings multirow mhchem siunitx enumitem
