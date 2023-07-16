b480a4e89580989a3ce887a314e0a28aba7a860d
nomutin <nomura0508@icloud.com>
2023-07-17 00:46:53 +0900

    [add] add workflow

---
+++ scripts/deploy.sh
@@ -0,0 +1,5 @@
+DOT_FILES=(.gitconfig .zshrc .gitignore .vimrc .vim .latexmkrc)
+for file in ${DOT_FILES[@]};
+do
+    ln -sf $HOME/dotfiles/$file $HOME/$file
+done
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,22 +1,8 @@
-#!/bin/bash
+#!/bin/zsh
 
-#"===== brew install ====="
-xcode-select --install
+set -eu
 
-if !(type 'brew' > /dev/null 2>&1); then
-    xcode-select --install
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi	
-
-#"===== make dotfiles alias ====="
-DOT_FILES=(.gitconfig .zshrc .gitignore .vimrc .vim .latexmkrc)
-for file in ${DOT_FILES[@]};
-do
-    ln -sf $HOME/dotfiles/$file $HOME/$file
-done
-
-#"===== startup apps =====
-brew install --cask iterm2 deepl coteditor visual-studio-code keyboardcleantool dozer mathpix-snipping-tool google-drive zoom arc raycast slack discord qlmarkdown
+brew bundle install --file="${HOME}"/dotfiles/.Brewfile
 
 #"===== MacBook settings =====
 sudo scutil --set HostName MBA
@@ -37,17 +23,6 @@ defaults write com.apple.finder ShowRecentTags -int 0
 defaults write com.apple.finder ShowMountedServersOnDesktop -int 0
 defaults write com.apple.finder SidebarTagsSctionDisclosedState -int 1
 defaults write com.apple.finder PreferencesWindow.LastSelection -string SDBR
-
-open /Applications/DeepL.app
-open /Applications/Raycast.app
-open /Applications/"Visual Studio Code.app"
-open /Applications/KeyboardCleanTool.app
-open /Applications/"Mathpix Snipping Tool.app"
-open /Applications/"Google Drive.app"
-open /Applications/Discord.app
-open /Applications/Slack.app
-open /Applications/zoom.us.app
-open /Applications/arc.app
 
 killall Finder
 killall Dock
