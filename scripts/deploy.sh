1627b1ca4d5e085aec789c60463acb6a79450dbc
nomutin <nomura0508@icloud.com>
2023-12-29 03:56:43 +0900

    :heavy_minus_sign: `Brewfile`の不要なライブラリを削除

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=(.gitconfig .zshrc .latexmkrc)
+DOT_FILES=(.gitconfig .zshrc)
 for file in "${DOT_FILES[@]}"; do
   ln -sf "$HOME"/dotfiles/config/"$file" "$HOME"/"$file"
 done
