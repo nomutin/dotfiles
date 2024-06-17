0319715eca62f31f230d83bd71ea0e3d22322d3b
nomutin <nomura0508@icloud.com>
2024-06-17 19:47:41 +0900

    [fix] macosで動作確認

--- scripts/deploy-linux.sh
+++ scripts/deploy-linux.sh
@@ -2,14 +2,17 @@
 
 set -eu
 
-cd "${HOME}/.dotfiles"
-
-ln -sf "${HOME}"/.dotfiles/config/.gitconfig "${HOME}"/.gitconfig
+ln -sf "${HOME}"/.dotfiles/config/git/.gitconfig "${HOME}"/.gitconfig
 ln -sf "${HOME}"/.dotfiles/config/vim/.vimrc "${HOME}"/.vimrc
-ln -sf "${HOME}"/.dotfiles/config/zellij/confg.kdl "${HOME}"/.config/zellij/config.kdl
+
+mkdir -p "${HOME}"/.config/zellij
+ln -sf "${HOME}"/.dotfiles/config/zellij/config.kdl "${HOME}"/.config/zellij/config.kdl
+
+mkdir -p "${HOME}"/.config/nvim
 ln -sf "${HOME}"/.dotfiles/config/neovim/init.lua "${HOME}"/.config/nvim/init.lua
+
+mkdir -p "${HOME}"/.config/mise
 ln -sf "${HOME}"/.dotfiles/config/mise/config.toml "${HOME}"/.config/mise/config.toml
 
-ln -sf "${HOME}"/.dotfiles/config/.profile "${HOME}"/.profile
-ln -sf "${HOME}"/.dotfiles/config/.bashrc "${HOME}"/.bashrc
+ln -sf "${HOME}"/.dotfiles/config/shell/.profile "${HOME}"/.profile
 
