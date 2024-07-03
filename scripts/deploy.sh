881c060c0253fb331c849c9047d86c85fb8993e5
nomutin <nomura0508@icloud.com>
2024-07-03 21:49:15 +0900

    [fix] vimをXGD_CONFIG_HOME参照するように

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,10 +2,6 @@
 
 set -eu
 
-# vim 9.1.0327 からは XDG_CONFIG_HOME に置けるみたい -> TODO: mise でインストールする?
-# https://github.com/vim/vim/commit/c9df1fb35a1866901c32df37dd39c8b39dbdb64a
-ln -sf "${HOME}"/.dotfiles/config/vim/.vimrc "${HOME}"/.vimrc
-
 mkdir -p "${HOME}"/.config/zellij
 ln -sf "${HOME}"/.dotfiles/config/zellij/config.kdl "${HOME}"/.config/zellij/config.kdl
 
@@ -20,6 +16,9 @@ ln -sf "${HOME}"/.dotfiles/config/mise/config.toml "${HOME}"/.config/mise/config
 
 mkdir -p "${HOME}"/.config/wezterm
 ln -sf "${HOME}"/.dotfiles/config/wezterm/wezterm.lua "${HOME}"/.config/wezterm/wezterm.lua
+
+mkdir -p "${HOME}"/.config/vim
+ln -sf "${HOME}"/.dotfiles/config/vim/vimrc "${HOME}"/.config/vim/vimrc
 
 if [ "$(uname)" = "Darwin" ]; then
   ln -sf "${HOME}"/.dotfiles/config/zsh/.zshrc "${HOME}"/.zshrc
