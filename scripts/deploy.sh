fc2c0628773e44c9c2ffb1bfbb81024d13421ed5
nomutin <nomura0508@icloud.com>
2024-07-03 23:49:36 +0900

    [fix] deployを修正

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,33 +2,22 @@
 
 set -eu
 
-mkdir -p "${HOME}"/.config/zellij
-ln -sf "${HOME}"/.dotfiles/config/zellij/config.kdl "${HOME}"/.config/zellij/config.kdl
+source_config_dir="${HOME}"/.dotfiles/config
+xdg_config_dir="${HOME}"/.config
 
-mkdir -p "${HOME}"/.config/git
-ln -sf "${HOME}"/.dotfiles/config/git/config "${HOME}"/.config/git/config
+mkdir -p "$xdg_config_dir"
 
-mkdir -p "${HOME}"/.config/nvim
-ln -sf "${HOME}"/.dotfiles/config/nvim/init.lua "${HOME}"/.config/nvim/init.lua
-
-mkdir -p "${HOME}"/.config/mise
-ln -sf "${HOME}"/.dotfiles/config/mise/config.toml "${HOME}"/.config/mise/config.toml
-
-mkdir -p "${HOME}"/.config/wezterm
-ln -sf "${HOME}"/.dotfiles/config/wezterm/wezterm.lua "${HOME}"/.config/wezterm/wezterm.lua
-
-mkdir -p "${HOME}"/.config/vim
-ln -sf "${HOME}"/.dotfiles/config/vim/vimrc "${HOME}"/.config/vim/vimrc
-
-mkdir -p "${HOME}"/.config/zsh
-ln -sf "${HOME}"/.dotfiles/config/zsh/.zshrc "${HOME}"/.config/zsh/.zshrc
+for item in "$source_config_dir"/*; do
+    base_item=$(basename "$item")
+    link_name="$xdg_config_dir/$base_item"
+    ln -s "$item" "$link_name"
+done
 
 if [ "$(uname)" = "Darwin" ]; then
-
+  mkdir -p "${HOME}"/.local/state/zsh "${HOME}"/.cache/zsh
   if ! grep -q 'export ZDOTDIR="$HOME"/.config/zsh' /etc/zshenv; then
     echo 'export ZDOTDIR="$HOME"/.config/zsh' | sudo tee -a /etc/zshenv
   fi
-  mkdir -p "${HOME}"/.local/state/zsh "${HOME}"/.cache/zsh
 
 elif [ "$(uname)" = "Linux" ]; then
   echo 'eval "$(~/.local/bin/mise activate bash)"' >>~/.bashrc
