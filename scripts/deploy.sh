42659d5e39939ed940680b814bd3d395f8c0d5ad
nomutin <nomura0508@icloud.com>
2024-07-03 22:39:51 +0900

    [fix] zshrc

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,16 +20,18 @@ ln -sf "${HOME}"/.dotfiles/config/wezterm/wezterm.lua "${HOME}"/.config/wezterm/
 mkdir -p "${HOME}"/.config/vim
 ln -sf "${HOME}"/.dotfiles/config/vim/vimrc "${HOME}"/.config/vim/vimrc
 
+mkdir -p "${HOME}"/.config/zsh
+ln -sf "${HOME}"/.dotfiles/config/zsh/.zshrc "${HOME}"/.config/zsh/.zshrc
+
 if [ "$(uname)" = "Darwin" ]; then
-  ln -sf "${HOME}"/.dotfiles/config/zsh/.zshrc "${HOME}"/.zshrc
-  # shellcheck disable=SC1090
-  source ~/.zshrc
+
+  if ! grep -q 'export ZDOTDIR="$HOME"/.config/zsh' /etc/zshenv; then
+    echo 'export ZDOTDIR="$HOME"/.config/zsh' | sudo tee -a /etc/zshenv
+  fi
   mkdir -p "${HOME}"/.local/state/zsh "${HOME}"/.cache/zsh
 
 elif [ "$(uname)" = "Linux" ]; then
-  # shellcheck disable=SC2016
   echo 'eval "$(~/.local/bin/mise activate bash)"' >>~/.bashrc
-  # shellcheck disable=SC2016
   echo 'source "$HOME/.rye/env"' >>~/.bashrc
   # shellcheck disable=SC1090
   source ~/.bashrc
