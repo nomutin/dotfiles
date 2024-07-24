5f2a2f8b6d79306a0b10a4221d2971794ccf29d3
nomutin <nomura0508@icloud.com>
2024-07-24 18:50:26 +0900

    [nvim] minimize plugins

--- xdg_config/nvim/init.lua
+++ xdg_config/nvim/init.lua
@@ -3,31 +3,39 @@
 -- ====== OPTIONS ======
 vim.loader.enable()
 vim.g.mapleader = " "
-vim.opt.title = true -- ウィンドウのタイトルを現在開いているファイル名で更新
-vim.opt.termguicolors = true -- ターミナルの色を24ビットカラーに設定
-vim.opt.clipboard = "unnamedplus" -- システムのクリップボードを使用
-vim.opt.completeopt = { "menuone", "noselect" } -- 補完メニューを表示し、自動で選択しない
-vim.opt.ignorecase = true -- 検索時に大文字小文字を区別しない
-vim.opt.pumheight = 10 -- ポップアップメニューの高さを10行に設定
-vim.opt.showtabline = 2 -- タブラインを常に表示
-vim.opt.ignorecase = true -- 検索時に大文字小文字を区別しない
-vim.opt.smartcase = true -- 検索パターンに大文字が含まれている場合は大文字小文字を区別
-vim.opt.smartindent = true -- 自動インデントを有効に
-vim.opt.undofile = true -- アンドゥ情報をファイルに保存
-vim.opt.expandtab = true -- タブをスペースに展開
-vim.opt.cursorline = true -- カーソル行をハイライト
-vim.opt.number = true -- 行番号を表示
-vim.opt.wrap = false -- 折り返しを無効に
-vim.opt.scrolloff = 8 -- スクロール時に画面の端から8行分余裕を持たせる
-vim.opt.sidescrolloff = 8 -- スクロール時に画面の端から8列分余裕を持たせる
-vim.opt.laststatus = 3 -- 最後のウィンドウのステータスラインを常に表示
-vim.opt.list = true -- 制御文字を表示
+vim.opt.title = true
+vim.opt.termguicolors = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.completeopt = { "menuone", "noselect", "popup"}
+vim.opt.ignorecase = true
+vim.opt.pumheight = 10
+vim.opt.showtabline = 2
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.undofile = true
+vim.opt.expandtab = true
+vim.opt.cursorline = true
+vim.opt.number = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.laststatus = 3
+vim.opt.list = true
 
 -- ====== KEYMAP ======
-vim.keymap.set("i", "jk", "<ESC>") -- インサートモード時 jk でノーマルモードに戻る
-vim.keymap.set("t", "fd", [[<C-\><C-n>]]) -- Terminal Mode 時 fd でノーマルモードに戻る
-vim.keymap.set("x", "<M-k>", ":move '<-2<CR>gv=gv") -- 選択範囲を上に移動
-vim.keymap.set("x", "<M-j>", ":move '>+1<CR>gv=gv") -- 選択範囲を下に移動
+vim.keymap.set("i", "jk", "<ESC>", { desc = "Restore from insert mode" })
+vim.keymap.set("t", "fd", [[<C-\><C-n>]], { desc = "Restore from terminal mode" })
+vim.keymap.set("n", "<leader>n", ":Lexplore<CR>", { desc = "Open Netrw" })
+
+-- ====== NETRW ======
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_showhide = 1
+vim.g.netrw_browse_split = 4
+vim.g.netrw_altv = 1
+vim.g.netrw_winsize = -28
+vim.g.netrw_keepdir = 1
 
 -- ====== COLORS ======
 vim.api.nvim_set_hl(0, "Function", { fg = "NvimLightBlue" })
@@ -38,95 +46,104 @@ vim.api.nvim_set_hl(0, "Special", { link = "Constant" })
 vim.api.nvim_set_hl(0, "@string.documentation", { fg = "NvimLightGreen", bold = true })
 vim.api.nvim_set_hl(0, "@variable.parameter", { fg = "NvimLightCyan", italic = true })
 
+-- ====== Git Gutter ======
+local function place_signs(name, start, lines, buffer)
+  for lnum = start, start + lines - 1 do
+    vim.fn.sign_place(0, '', name, buffer, { lnum = lnum })
+  end
+end
+
+local function put_signs(diff)
+  local _, old_lines, new_start, new_lines = unpack(diff)
+  local buffer = vim.api.nvim_get_current_buf()
+  if old_lines == 0 then
+    place_signs('HunkSignAdd', new_start, new_lines, buffer)
+  elseif new_lines == 0 then
+    place_signs('HunkSignDel', new_start, old_lines, buffer)
+  else
+    place_signs('HunkSignUpd', new_start, math.min(old_lines, new_lines), buffer)
+    place_signs('HunkSignDel', new_start + new_lines, old_lines - new_lines, buffer)
+    place_signs('HunkSignAdd', new_start + old_lines, new_lines - old_lines, buffer)
+  end
+end
+
+local function show_hunk()
+  vim.fn.sign_define('HunkSignAdd', { text = '+', texthl = 'DiffAdd' })
+  vim.fn.sign_define('HunkSignDel', { text = '-', texthl = "DiffDelete" })
+  vim.fn.sign_define('HunkSignUpd', { text = '~', texthl = "DiffChange" })
+  local cmd = 'git --no-pager diff -U0 --no-color --no-ext-diff ' .. vim.fn.expand('%')
+              .. ' | grep "^@@" '
+              .. ' | sed -r "s/[-+]([0-9]+) /\\1,1,/g" '
+              .. ' | sed -r "s/^[-@ ]*([0-9]+,[0-9]+)[ ,+]+([0-9]+,[0-9]+)[, ].*/\\1,\\2/"'
+  local output = vim.fn.systemlist(cmd)
+  for _, line in ipairs(output) do
+    put_signs(vim.split(line, ','))
+  end
+end
+vim.api.nvim_create_autocmd( {"BufReadPost", "BufWritePost"}, { callback = show_hunk })
+
+-- ====== COMPLETION ======
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client and client.supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+    end
+    local function trigger_completion_and_insert_space()
+      vim.lsp.completion.trigger()
+      vim.api.nvim_feedkeys(" ", "n", true)
+    end
+    vim.keymap.set("i", " ", trigger_completion_and_insert_space)
+    vim.keymap.set("i", "<C-j>", vim.lsp.completion.trigger )
+  end,
+})
+
 -- ====== PLUGIN ======
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 local lazyrepo = "https://github.com/folke/lazy.nvim.git"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.runtimepath:prepend(lazypath)
 
 require("lazy").setup({
-  { "github/copilot.vim", event = "BufRead" },
-  {
-    "folke/flash.nvim",
-    keys = {
-      { "s", mode = { "n", "x", "o" }, "<cmd>lua require('flash').jump()<cr>" },
-      { "S", mode = { "n", "x", "o" }, "<cmd>lua require('flash').treesitter()<cr>" },
-    },
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    event = "BufRead",
-    keys = {
-      { "<leader>hd", mode = "n", "<cmd>Gitsigns diffthis<cr>" },
-      { "<leader>hp", mode = "n", "<cmd>Gitsigns preview_hunk<cr>" },
-    },
-    opts = {},
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "kyazdani42/nvim-web-devicons" },
-    keys = { { "<leader>n", mode = "n", "<cmd>NvimTreeToggle<cr>" } },
-    opts = {},
-  },
-  { "nvim-lualine/lualine.nvim", event = "BufRead", opts = {} },
-  {
-    "williamboman/mason.nvim",
-    event = "BufRead",
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "jay-babu/mason-null-ls.nvim",
-      "nvimtools/none-ls.nvim",
-      "neovim/nvim-lspconfig",
-      "hrsh7th/nvim-cmp",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-path",
-    },
-    config = function()
-      require("mason").setup()
-      require("mason-lspconfig").setup()
-      require("mason-null-ls").setup({ handlers = {} })
-      require("null-ls").setup()
-      require("mason-lspconfig").setup_handlers({
-        function(server_name)
-          require("lspconfig")[server_name].setup({
-            capabilities = require("cmp_nvim_lsp").default_capabilities(),
-          })
-        end,
-      })
-      local cmp = require("cmp")
-      cmp.setup({
-        mapping = cmp.mapping.preset.insert({}),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "path" },
-        }),
-      })
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = "BufRead",
-    main = "nvim-treesitter.configs",
-    opts = { highlight = { enable = true }, indent = { enable = true } },
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    keys = {
-      { "<leader>ff", mode = "n", "<cmd>Telescope find_files<cr>" },
-      { "<leader>fg", mode = "n", "<cmd>Telescope live_grep<cr>" },
-      { "<leader>fb", mode = "n", "<cmd>Telescope buffers<cr>" },
-    },
-  },
-  {
-    "akinsho/toggleterm.nvim",
-    keys = {
-      { "<leader>tt", "<cmd>ToggleTerm direction=float<cr>" },
-      { "<leader>tj", "<cmd>ToggleTerm direction=horizontal<cr>" },
-    },
-    opts = {},
-  },
   defaults = { lazy = true },
+  spec = {
+    { "github/copilot.vim", event = "BufRead" },
+    {
+      "folke/flash.nvim",
+      keys = {
+        { "s", mode = { "n", "x", "o" }, "<cmd>lua require('flash').jump()<cr>" },
+        { "S", mode = { "n", "x", "o" }, "<cmd>lua require('flash').treesitter()<cr>" },
+      },
+    },
+    { "nvim-lualine/lualine.nvim", event = "BufRead", opts = {} },
+    {
+      "nvim-lspconfig",
+      event = "BufRead",
+      config = function()
+        local server_names = {
+          "bashls", "biome", "cssls", "dockerls", "html", "jsonls", "luals",
+          "marksman", "pyright", "ruff", "rust_analyzer", "taplo", "tsserver", "yamlls",
+        }
+        for _, server_name in ipairs(server_names) do
+          require("lspconfig")[server_name].setup({})
+        end
+      end,
+    },
+    {
+      "nvim-treesitter/nvim-treesitter",
+      event = "BufRead",
+      main = "nvim-treesitter.configs",
+      opts = { highlight = { enable = true }, indent = { enable = true } },
+    },
+    {
+      "nvim-telescope/telescope.nvim",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      keys = {
+        {"<leader>ff", mode = "n", "<cmd>Telescope find_files<cr>" },
+        {"<leader>fg", mode = "n", "<cmd>Telescope live_grep<cr>" },
+      },
+    },
+  },
 })
