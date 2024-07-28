8abfa2a1cfd1f96f6ed78471612281d29df9d01c
nomutin <nomura0508@icloud.com>
2024-07-28 15:00:38 +0900

    [zellij] ml修正

--- xdg_config/nvim/init.lua
+++ xdg_config/nvim/init.lua
@@ -3,20 +3,10 @@
 -- ====== BUILTIN ======
 vim.loader.enable()
 vim.g.mapleader = " "
-
-vim.g.netrw_banner = 0
-vim.g.netrw_liststyle = 3
-vim.g.netrw_browse_split = 4
-vim.g.showhide = 1
-vim.g.netrw_altv = 1
-vim.g.netrw_winsize = -28
-vim.g.netrw_keepdir = 1
-vim.g.netrw_preview = 1
-
 vim.opt.title = true
 vim.opt.termguicolors = true
 vim.opt.clipboard = "unnamedplus"
-vim.opt.completeopt = { "menuone", "noselect" }
+vim.opt.completeopt = { "menu", "menuone", "noselect", "fuzzy", "popup" }
 vim.opt.ignorecase = true
 vim.opt.pumheight = 10
 vim.opt.showtabline = 2
@@ -33,9 +23,18 @@ vim.opt.sidescrolloff = 8
 vim.opt.laststatus = 3
 vim.opt.list = true
 
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_browse_split = 4
+vim.g.showhide = 1
+vim.g.netrw_altv = 1
+vim.g.netrw_winsize = -28
+vim.g.netrw_keepdir = 1
+vim.g.netrw_preview = 1
+
 vim.api.nvim_set_hl(0, "Function", { fg = "NvimLightBlue" })
 vim.keymap.set("i", "jk", "<ESC>", { desc = "Return to normal mode" })
-vim.keymap.set("t", "fd", [[<C-\><C-n>]])
+vim.keymap.set("t", "fd", [[<C-\><C-n>]], { desc = "Return to normal mode" })
 vim.keymap.set("n", "<leader>n", "<cmd>Lexplore<cr>", { desc = "Open file explorer" })
 
 -- ====== PLUGIN ======
@@ -45,57 +44,51 @@ if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
+local servers = { "bashls", "biome", "jsonls", "lua_ls", "pyright", "ruff", "rust_analyzer", "yamlls" }
 
 require("lazy").setup({
-  { "github/copilot.vim", event = "BufRead" },
-  {
-    "folke/flash.nvim",
-    keys = {
-      { "s", mode = { "n", "x", "o" }, "<cmd>lua require('flash').jump()<cr>", desc = "Flash" },
-      { "S", mode = { "n", "x", "o" }, "<cmd>lua require('flash').treesitter()<cr>", desc = "Flash Treesitter" },
-    },
-  },
-  { "lewis6991/gitsigns.nvim", event = "BufRead", opts = {} },
-  { "nvim-lualine/lualine.nvim", event = "BufRead", opts = {} },
-  {
-    "williamboman/mason.nvim",
-    event = "BufRead",
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "neovim/nvim-lspconfig",
-      "hrsh7th/nvim-cmp",
-      "hrsh7th/cmp-nvim-lsp",
-    },
-    config = function()
-      require("mason").setup()
-      require("mason-lspconfig").setup()
-      require("mason-lspconfig").setup_handlers({
-        function(server_name)
-          require("lspconfig")[server_name].setup({
-            capabilities = require("cmp_nvim_lsp").default_capabilities(),
-          })
-        end,
-      })
-      local cmp = require("cmp")
-      cmp.setup({
-        mapping = cmp.mapping.preset.insert({}),
-        sources = cmp.config.sources({ { name = "nvim_lsp" } }),
-      })
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = "BufRead",
-    main = "nvim-treesitter.configs",
-    opts = { highlight = { enable = true }, indent = { enable = true } },
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    keys = {
-      { "<leader>f", "<cmd>Telescope find_files<cr>", { desc = "Find files" } },
-      { "<leader>g", "<cmd>Telescope live_grep<cr>", { desc = "Live Grep" } },
-    },
-  },
   defaults = { lazy = true },
+  rocks = { enabled = false },
+  spec = {
+    { "github/copilot.vim", event = "BufRead" },
+    {
+      "folke/flash.nvim",
+      keys = {
+        { "s", mode = { "n", "x", "o" }, "<cmd>lua require('flash').jump()<cr>", desc = "Flash" },
+        { "S", mode = { "n", "x", "o" }, "<cmd>lua require('flash').treesitter()<cr>", desc = "Flash Treesitter" },
+      },
+    },
+    { "lewis6991/gitsigns.nvim", event = "BufRead", opts = {} },
+    { "nvim-lualine/lualine.nvim", event = "BufRead", opts = {} },
+    {
+      "nvim-lspconfig",
+      event = "BufRead",
+      dependencies = { "hrsh7th/nvim-cmp", "hrsh7th/cmp-nvim-lsp" },
+      config = function()
+        for _, server in ipairs(servers) do
+          local capabilities = require("cmp_nvim_lsp").default_capabilities()
+          require("lspconfig")[server].setup({ capabilities = capabilities })
+        end
+        local cmp = require("cmp")
+        cmp.setup({
+          mapping = cmp.mapping.preset.insert({}),
+          sources = cmp.config.sources({ { name = "nvim_lsp" } }),
+        })
+      end,
+    },
+    {
+      "nvim-treesitter/nvim-treesitter",
+      event = "BufRead",
+      main = "nvim-treesitter.configs",
+      opts = { highlight = { enable = true }, indent = { enable = true } },
+    },
+    {
+      "nvim-telescope/telescope.nvim",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      keys = {
+        { "<leader>f", "<cmd>Telescope find_files<cr>", { desc = "Find files" } },
+        { "<leader>g", "<cmd>Telescope live_grep<cr>", { desc = "Live Grep" } },
+      },
+    },
+  },
 })
