2942cf65f6a34cec35efd018c6ee8447de7e6ab9
nomutin <nomura0508@icloud.com>
2024-08-06 22:44:07 +0900

    [sh] `zsh` -> `bash`

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,24 +4,35 @@ set -eu
 
 cd "${HOME}"
 
-# Install my dotfiles
+# ===== Install repo =====
 if [ ! -d .dotfiles ]; then
   git clone https://github.com/nomutin/dotfiles.git .dotfiles
 fi
 
-# Install mise
+# ===== Install mise =====
 if ! (type 'mise' >/dev/null 2>&1); then
   curl https://mise.run | sh
 fi
 
-cd .dotfiles
+# ===== Deploy xdg-based configs =====
+src="${HOME}/.dotfiles/xdg_config"
+tgt="${HOME}/.config"
+mkdir -p "$tgt"
+for item in "$src"/*; do
+  ln -s "$item" "$tgt/$(basename "$item")"
+done
 
-# Deploy
-make deploy
+# ===== Deploy bashrc =====
+if [ -f "${HOME}"/.bashrc ]; then
+  echo 'source "$HOME/.dotfiles/config/bashrc"' >>~/.bashrc
+else
+  ln -s "${HOME}"/.dotfiles/config/bashrc "${HOME}"/.bashrc
+fi
 
+# ===== Install dependencies =====
 mise install -y
 
-# Set up macOS
+# ===== Set up Macos =====
 if [ "$(uname)" = "Darwin" ]; then
-  make init_macos
+  bash scripts/init_macos.sh
 fi
--- xdg_config/nvim/init.lua
+++ xdg_config/nvim/init.lua
@@ -1,6 +1,7 @@
 -- Minimalist neovim configuration by @nomutin
+-- Requires: neovim>=0.10.0, git>=2.31.0, npm(lsp), ripgrep(telescope),
 
--- ====== BUILTIN ======
+-- ====== OPTIONS ======
 vim.loader.enable()
 vim.g.mapleader = " "
 vim.opt.title = true
@@ -22,7 +23,12 @@ vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
 vim.opt.laststatus = 3
 vim.opt.list = true
+vim.opt.path = "**"
 
+-- ====== COLORS ======
+vim.api.nvim_set_hl(0, "Type", { fg = "NvimLightBlue" })
+
+-- ====== NETRW ======
 vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
 vim.g.netrw_browse_split = 4
@@ -32,10 +38,13 @@ vim.g.netrw_winsize = -28
 vim.g.netrw_keepdir = 1
 vim.g.netrw_preview = 1
 
-vim.api.nvim_set_hl(0, "Function", { fg = "NvimLightBlue" })
+-- ====== KEYMAP ======
 vim.keymap.set("i", "jk", "<ESC>", { desc = "Return to normal mode" })
 vim.keymap.set("t", "fd", [[<C-\><C-n>]], { desc = "Return to normal mode" })
 vim.keymap.set("n", "<leader>n", "<cmd>Lexplore<cr>", { desc = "Open file explorer" })
+
+-- ====== COMMAND ======
+vim.api.nvim_create_user_command('ConfigOpen', 'e $MYVIMRC', {})
 
 -- ====== PLUGIN ======
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -44,51 +53,48 @@ if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
-local servers = { "bashls", "biome", "jsonls", "lua_ls", "pyright", "ruff", "rust_analyzer", "yamlls" }
+local servers = { "bashls", "biome", "jsonls", "lua_ls", "basedpyright", "ruff", "rust_analyzer", "yamlls" }
 
 require("lazy").setup({
   defaults = { lazy = true },
-  rocks = { enabled = false },
-  spec = {
-    { "github/copilot.vim", event = "BufRead" },
-    {
-      "folke/flash.nvim",
-      keys = {
-        { "s", mode = { "n", "x", "o" }, "<cmd>lua require('flash').jump()<cr>", desc = "Flash" },
-        { "S", mode = { "n", "x", "o" }, "<cmd>lua require('flash').treesitter()<cr>", desc = "Flash Treesitter" },
-      },
+  { "github/copilot.vim", event = "BufRead" },
+  {
+    "folke/flash.nvim",
+    keys = {
+      { "s", mode = { "n", "x", "o" }, "<cmd>lua require('flash').jump()<cr>", desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, "<cmd>lua require('flash').treesitter()<cr>", desc = "Flash Treesitter" },
     },
-    { "lewis6991/gitsigns.nvim", event = "BufRead", opts = {} },
-    { "nvim-lualine/lualine.nvim", event = "BufRead", opts = {} },
-    {
-      "nvim-lspconfig",
-      event = "BufRead",
-      dependencies = { "hrsh7th/nvim-cmp", "hrsh7th/cmp-nvim-lsp" },
-      config = function()
-        for _, server in ipairs(servers) do
-          local capabilities = require("cmp_nvim_lsp").default_capabilities()
-          require("lspconfig")[server].setup({ capabilities = capabilities })
-        end
-        local cmp = require("cmp")
-        cmp.setup({
-          mapping = cmp.mapping.preset.insert({}),
-          sources = cmp.config.sources({ { name = "nvim_lsp" } }),
-        })
-      end,
-    },
-    {
-      "nvim-treesitter/nvim-treesitter",
-      event = "BufRead",
-      main = "nvim-treesitter.configs",
-      opts = { highlight = { enable = true }, indent = { enable = true } },
-    },
-    {
-      "nvim-telescope/telescope.nvim",
-      dependencies = { "nvim-lua/plenary.nvim" },
-      keys = {
-        { "<leader>f", "<cmd>Telescope find_files<cr>", { desc = "Find files" } },
-        { "<leader>g", "<cmd>Telescope live_grep<cr>", { desc = "Live Grep" } },
-      },
+  },
+  { "lewis6991/gitsigns.nvim", event = "BufRead", opts = {} },
+  { "nvim-lualine/lualine.nvim", event = "BufRead", opts = {} },
+  {
+    "neovim/nvim-lspconfig",
+    event = "BufRead",
+    dependencies = { "hrsh7th/nvim-cmp", "hrsh7th/cmp-nvim-lsp" },
+    config = function()
+      for _, server in ipairs(servers) do
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        require("lspconfig")[server].setup({ capabilities = capabilities })
+      end
+      local cmp = require("cmp")
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({}),
+        sources = cmp.config.sources({ { name = "nvim_lsp" } }),
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = "BufRead",
+    main = "nvim-treesitter.configs",
+    opts = { highlight = { enable = true }, indent = { enable = true } },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>f", "<cmd>Telescope find_files<cr>", { desc = "Find files" } },
+      { "<leader>/", "<cmd>Telescope live_grep<cr>", { desc = "Live grep" } },
     },
   },
 })
