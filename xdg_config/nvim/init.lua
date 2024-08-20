225219b0c3ca4d30717540675dd6c885f0cb86b4
nomutin <nomura0508@icloud.com>
2024-08-20 10:14:45 +0900

    [nvim] clipboard

--- xdg_config/nvim/init.lua
+++ xdg_config/nvim/init.lua
@@ -7,40 +7,34 @@ vim.opt.title = true
 vim.opt.termguicolors = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.completeopt = { "menu", "menuone", "noselect", "popup" }
-vim.opt.ignorecase = true
 vim.opt.pumheight = 10
-vim.opt.showtabline = 2
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+vim.opt.ignorecase, vim.opt.smartcase = true, true
+vim.opt.scrolloff, vim.opt.sidescrolloff = 8, 8
+vim.opt.showtabline, vim.opt.laststatus = 2, 3
 vim.opt.smartindent = true
 vim.opt.undofile = true
 vim.opt.expandtab = true
 vim.opt.cursorline = true
 vim.opt.number = true
 vim.opt.wrap = false
-vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 8
-vim.opt.laststatus = 3
 vim.opt.list = true
 vim.opt.path = "**"
-vim.opt.diffopt:append("vertical,context:999999")
 vim.api.nvim_set_hl(0, "Type", { fg = "NvimLightBlue" })
 
 -- ====== NETRW ======
 vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
-vim.g.netrw_browse_split = 4
 vim.g.showhide = 1
 vim.g.netrw_altv = 1
 vim.g.netrw_winsize = -28
 vim.g.netrw_keepdir = 1
 
 -- ====== GIT DIFF ======
-function ShowGitDiff()
+local function show_git_diff(_)
   local diff_file = vim.fn.expand("%:h") .. "/__" .. vim.fn.expand("%:t")
-  vim.cmd(string.format("silent !git show HEAD:%s > %s", vim.fn.expand("%"), diff_file))
-  vim.cmd(string.format("vertical diffsplit %s", diff_file))
-  vim.cmd(string.format("autocmd BufWinLeave <buffer> silent! !rm %s", diff_file))
+  vim.fn.system("git show HEAD:" .. vim.fn.expand("%") .. " > " .. diff_file)
+  vim.cmd("vertical diffsplit " .. diff_file)
+  vim.cmd("autocmd BufWinLeave <buffer> silent! !rm " .. diff_file)
 end
 
 -- ====== STATUSLINE ======
@@ -52,33 +46,43 @@ local function show_info(tbl)
   return #result > 0 and " [" .. table.concat(result, " ") .. "]" or ""
 end
 
-local function git_status()
+local function update_statusline(_)
   local branch = vim.fn.system("git rev-parse --abbrev-ref HEAD 2>/dev/null")
   local diff = vim.fn.system("git diff --numstat " .. vim.fn.expand("%") .. " 2>/dev/null")
   local added, deleted = diff:match("(%d+)%s+(%d+)%s+")
-  return branch == "" and "NULL" or branch .. show_info({ ["+"] = added, ["-"] = deleted })
-end
+  local git = branch == "" and "NULL" or branch .. show_info({ ["+"] = added, ["-"] = deleted })
 
-local function lsp_status()
   local clients = {}
   for _, client in ipairs(vim.lsp.get_clients({ bufnr = 0 })) do
     table.insert(clients, client.name)
   end
   local diag = vim.diagnostic.count(0)
   local count = { ["E:"] = diag[1], ["W:"] = diag[2], ["I:"] = diag[3], ["H:"] = diag[4] }
-  return #clients > 0 and table.concat(clients, ", ") .. show_info(count) or "NULL"
-end
+  local lsp = #clients > 0 and table.concat(clients, ", ") .. show_info(count) or "NULL"
 
-local function update_statusline()
-  local line = table.concat({ " %f%h%w%m%r", git_status(), "%=", lsp_status(), "%l:%c", "%P " }, " │ ")
-  vim.opt.statusline = line
+  vim.opt.statusline = " %f%h%w%m%r │ " .. git .. " │ %= │ " .. lsp .. " │ %l:%c │ %P "
 end
 vim.api.nvim_create_autocmd({ "VimEnter", "BufWritePost" }, { callback = update_statusline })
+
+-- ====== CLIPBOARD ======
+if os.getenv("SSH_CLIENT") ~= nil or os.getenv("SSH_TTY") ~= nil then
+  local function paste(_)
+    return function(_)
+      return vim.split(vim.fn.getreg('"'), "\n")
+    end
+  end
+  local osc52 = require("vim.ui.clipboard.osc52")
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = { ["+"] = osc52.copy("+"), ["*"] = osc52.copy("*") },
+    paste = { ["+"] = paste("+"), ["*"] = paste("*") },
+  }
+end
 
 -- ====== KEYMAP ======
 vim.keymap.set("i", "jk", "<ESC>")
 vim.keymap.set("n", "<leader>n", "<cmd>Lexplore<cr>")
-vim.keymap.set("n", "<leader>d", ShowGitDiff)
+vim.keymap.set("n", "<leader>d", show_git_diff)
 
 -- ====== PLUGIN ======
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -95,8 +99,8 @@ require("lazy").setup({
   {
     "folke/flash.nvim",
     keys = {
-      { "s", mode = { "n", "x", "o" }, "<cmd>lua require('flash').jump()<cr>" },
-      { "S", mode = { "n", "x", "o" }, "<cmd>lua require('flash').treesitter()<cr>" },
+      { "s", mode = { "n", "x" }, "<cmd>lua require('flash').jump()<cr>" },
+      { "S", mode = { "n", "x" }, "<cmd>lua require('flash').treesitter()<cr>" }
     },
   },
   {
